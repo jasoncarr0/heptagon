eaf15df37f064a3358b5748cc2b0179d62f19106
Jason Carr <jac3559@rit.edu>
2016-11-15 00:06:34 -0500

    Template changes

--- Heptagon/Templates.hs
+++ Heptagon/Templates.hs
@@ -8,11 +8,12 @@ import Data.Map
 import Text.Parsec
 import Heptagon.Templates.Inject
 
+data TemplateTerm = RawHTML String | VarInj String | Tag String
 
 
+varInject :: Stream s m t => ParsecT s u m TemplateTerm
+varInject = VarInj <$> (startVar >> manyTil (notFollowedBy startVar anyChar) endVar)
 
-applyKeys :: ValMap -> String -> String
-applyKeys vmap s = s
 
 initial :: Char
 initial = '{'
